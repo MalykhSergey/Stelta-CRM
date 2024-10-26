ba18d5495f6212add206148f283ab2f0188ea9ea
master_bit <sergeymalykhdm@gmail.com>
2024-10-26 17:59:57 +0600

    updateForms

--- src/app/components/DocumentForm/DocumentForm.tsx
+++ src/app/components/DocumentForm/DocumentForm.tsx
@@ -2,11 +2,14 @@ import { faCaretUp } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { observer, useLocalObservable } from 'mobx-react-lite';
 import { Tender } from '../../models/Tender';
-import styles from './DocumentFrom.module.css';
+import styles from './DocumentForm.module.css';
 interface DocumentsFormProps {
-    tender: Tender
+    tender: Tender,
+    title: string,
+    isEditable: boolean,
 }
-const DocumentsForm: React.FC<DocumentsFormProps> = observer(({ tender }) => {
+const DocumentsForm: React.FC<DocumentsFormProps> = observer(({ tender, title, isEditable }) => {
+    console.log('redraw')
     let collapsed = useLocalObservable(() => ({
         isTrue: true,
         toggle() { this.isTrue = !this.isTrue }
@@ -23,12 +26,14 @@ const DocumentsForm: React.FC<DocumentsFormProps> = observer(({ tender }) => {
         files.push(<p key={fileName + files.length}><a href={`/download/${fileName}`} download>{fileName}</a></p>)
     }
     return (
-        <div className={`card ${styles.form} ${collapsed.isTrue ? styles.expanded : ''}`}><h3>Файлы <button className={styles.button} onClick={collapsed.toggle}><FontAwesomeIcon icon={faCaretUp} className={`${styles.icon} ${!collapsed.isTrue ? styles.rotated : ''}`} /></button></h3>
-            <form>
-                {files}
-                <input type="file" name="file" multiple />
-                <input type="hidden" name="tenderId" value={tender.id} />
-            </form>
+        <div className={`card ${styles.form} ${collapsed.isTrue ? styles.expanded : ''}`}><h3>{title} <button className={styles.toggler} onClick={collapsed.toggle}><FontAwesomeIcon icon={faCaretUp} className={`${styles.icon} ${!collapsed.isTrue ? styles.rotated : ''}`} /></button></h3>
+            {files}
+            {isEditable &&
+                <form>
+                    <input type="file" name="file" multiple />
+                    <input type="hidden" name="tenderId" value={tender.id} />
+                </form>
+            }
         </div>
     )
 });
