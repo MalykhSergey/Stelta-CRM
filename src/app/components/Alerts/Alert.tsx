f61d01650b17fbe95ca0e13fc1e67da3496a28ce
master_bit <sergeymalykhdm@gmail.com>
2024-11-21 20:56:40 +0600

    Update alerts

--- src/app/components/Alerts/Alert.tsx
+++ src/app/components/Alerts/Alert.tsx
@@ -1,19 +1,22 @@
 import { faCheck, faExclamation } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "./Alert.module.css";
-let timer_1: NodeJS.Timeout | undefined;
-let timer_2: NodeJS.Timeout | undefined;
-export const showMessage = (message: string, type:string = 'error') => {
+let timers: { [x: string]: (NodeJS.Timeout | number)[] } = {
+    error: [0, 0],
+    successful: [0, 0],
+};
+export const showMessage = (message: string, type: string = 'error') => {
     const alert = document.getElementById(`${type}-alert`)
     const alert_text = document.getElementById(`${type}-text`)
+    console.log(timers)
     if (alert && alert_text) {
         alert_text.textContent = message
         alert.style.display = "flex"
         alert.style.opacity = '1'
-        clearTimeout(timer_1)
-        clearTimeout(timer_2)
-        timer_1 = setTimeout(() => { alert.style.opacity = '0' }, 4000);
-        timer_2 = setTimeout(() => { alert.style.display = "none" }, 7000);
+        clearTimeout(timers[type][0])
+        clearTimeout(timers[type][1])
+        timers[type][0] = setTimeout(() => { alert.style.opacity = '0' }, 4000);
+        timers[type][1] = setTimeout(() => { alert.style.display = "none" }, 7000);
     }
 };
 
@@ -26,7 +29,7 @@ export const doWithErrorCheck = async (doFunc: () => any) => {
 
 export const AlertContainer = () => (
     <div className={styles.container}>
-        <div className={`${styles.error} ${styles.alert}`} id="error-alert"><FontAwesomeIcon icon={faExclamation} style={{ height: '20px'}}></FontAwesomeIcon><div id="error-text"></div></div>
-        <div className={`${styles.successful} ${styles.alert}`} id="successful-alert"><FontAwesomeIcon icon={faCheck} style={{ height: '20px'}}></FontAwesomeIcon><div id="successful-text"></div></div>
+        <div className={`${styles.error} ${styles.alert}`} id="error-alert"><FontAwesomeIcon icon={faExclamation} style={{ height: '20px' }}></FontAwesomeIcon><div id="error-text"></div></div>
+        <div className={`${styles.successful} ${styles.alert}`} id="successful-alert"><FontAwesomeIcon icon={faCheck} style={{ height: '20px' }}></FontAwesomeIcon><div id="successful-text"></div></div>
     </div>
 );
