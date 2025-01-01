a6a4c7927f5fc4b3d6fe8c5c36a826bfee9e0c88
sergey_malykh <sergeymalykhdm@gmail.com>
2025-01-01 20:14:26 +0600

    Исправить баг с загрузкой организации в тендере

--- src/app/tender/[tenderId]/TenderPageClientWrapper.tsx
+++ src/app/tender/[tenderId]/TenderPageClientWrapper.tsx
@@ -1,15 +1,16 @@
 'use client';
 
-import {ConfirmDialogProvider} from '@/app/components/Dialog/ConfirmDialogContext';
+import { ConfirmDialogProvider } from '@/app/components/Dialog/ConfirmDialogContext';
 import Company from '@/models/Company/Company';
-import {Tender} from '@/models/Tender/Tender';
+import { Tender } from '@/models/Tender/Tender';
 import TenderPageClient from './TenderPageClient';
 
-function TenderPageClientWrapper({tenderString, companiesString}: { tenderString: string, companiesString: string }) {
+function TenderPageClientWrapper({ tenderString, companiesString }: { tenderString: string, companiesString: string }) {
     const tender = Tender.fromJSON(tenderString)
-    const companies =Company.fromJSONArray(companiesString)
-    tender.company = companies.find(company => company.id === tender.company.id)!
-    return (<ConfirmDialogProvider><TenderPageClient tender={tender} companies={companies}/></ConfirmDialogProvider>)
+    const companies = Company.fromJSONArray(companiesString)
+    if (tender.company.id !=0)
+        tender.company = companies.find(company => company.id === tender.company.id)!
+    return (<ConfirmDialogProvider><TenderPageClient tender={tender} companies={companies} /></ConfirmDialogProvider>)
 }
 
 export default TenderPageClientWrapper;
