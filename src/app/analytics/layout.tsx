481d55c29ec6d87f241dfafd9ca32644c9a5b99e
sergey_malykh <sergeymalykhdm@gmail.com>
2025-04-15 01:23:04 +0600

    feat: добавить кнопку экспорт в Excel

--- src/app/analytics/layout.tsx
+++ src/app/analytics/layout.tsx
@@ -1,27 +1,33 @@
 "use client"
 import Link from 'next/link';
-import {usePathname} from 'next/navigation';
-import styles from  './layout.module.css'
+import {usePathname, useSearchParams} from 'next/navigation';
+import styles from './layout.module.css'
 import './table.css'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faFileExcel} from "@fortawesome/free-solid-svg-icons";
 
 export default function LayoutPage({children,}: {
     children: React.ReactNode
 }) {
     const pathname = usePathname()
+    const searchParams = useSearchParams()
 
     return (
         <>
             <div id={styles.linkBar}>
                 <Link href='./company'
-                      className={`${styles.link} ${pathname === '/analytics/company' ? styles.active : ''}`}>По организациям</Link>
+                      className={`${styles.link} ${pathname === '/analytics/company' ? styles.active : ''}`}>По
+                    организациям</Link>
                 <Link href='./tenders'
-                      className={`${styles.link} ${pathname === '/analytics/tenders' ? styles.active : ''}`}>Все тендеры</Link>
+                      className={`${styles.link} ${pathname === '/analytics/tenders' ? styles.active : ''}`}>Все
+                    тендеры</Link>
                 <Link href='./win_loose'
-                      className={`${styles.link} ${pathname === '/analytics/win_loose' ? styles.active : ''}`}>Победа / Поражение</Link>
+                      className={`${styles.link} ${pathname === '/analytics/win_loose' ? styles.active : ''}`}>Победа /
+                    Поражение</Link>
             </div>
-            {/*<div id={styles.content} className='shadowed'>*/}
-                {children}
-            {/*</div>*/}
+            <a className={styles.download} href={`../api${pathname}/${searchParams.toString()}`} download>Экспорт в
+                Excel <FontAwesomeIcon icon={faFileExcel}/></a>
+            {children}
         </>
     )
 }
--- src/app/api/analytics/[type]/route.ts
+++ src/app/api/analytics/[type]/route.ts
@@ -1,24 +1,43 @@
-import {NextResponse} from "next/server";
-import {getCompaniesFullAnalytics} from "@/models/Analytics/AnalyticsService";
-import CompanyAnalytics from "@/models/Analytics/Table/Excel/CompanyAnalytics";
+import {
+    getCompaniesFullAnalytics,
+    getCompaniesWinLooseAnalytics,
+    getTendersAnalytics
+} from "@/models/Analytics/AnalyticsService";
+import CompanyFullAnalytics from "@/models/Analytics/Table/Excel/CompanyFullAnalytics";
+import CompanyWinLooseAnalytics from "@/models/Analytics/Table/Excel/CompanyWinLooseAnalytics";
 
 export async function GET(
     request: Request,
     {params}: { params: { type: string } }
 ) {
-    const tableData = await getCompaniesFullAnalytics(false);
-    if (!tableData) {
-        return NextResponse.json(
-            {error: "Данные таблицы не найдены"},
-            {status: 404}
-        );
-    }
+    let tableData;
+    let table;
 
-    const table = new CompanyAnalytics(tableData.headers, tableData.data, tableData.colSizes);
-    return new Response(await table.render().xlsx.writeBuffer(), {
-        headers: {
-            "Content-Type": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-            "Content-Disposition": 'attachment; filename="analytics.xlsx"',
-        },
-    });
+    switch (params.type) {
+        case "company": {
+            tableData = await getCompaniesFullAnalytics(false)
+            table = new CompanyFullAnalytics(tableData.headers, tableData.data, tableData.colSizes);
+            break
+        }
+        case "win_loose": {
+            tableData = await getCompaniesWinLooseAnalytics(false)
+            table = new CompanyWinLooseAnalytics(tableData.headers, tableData.data, tableData.colSizes);
+            break
+        }
+        case "tenders": {
+            tableData = await getCompaniesWinLooseAnalytics(false)
+            break
+        }
+        default: {
+            return new Response("Неправильный параметр пути!", {status: 403});
+        }
+    }
+    if (tableData&&table) {
+        return new Response(await table.render().xlsx.writeBuffer(), {
+            headers: {
+                "Content-Type": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                "Content-Disposition": 'attachment; filename="analytics.xlsx"',
+            },
+        });
+    } else return new Response("Ошибка получения данных из БД!", {status: 500});
 }
