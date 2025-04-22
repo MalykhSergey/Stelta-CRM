8aaa840e382babbb7fd62f89484117bebd71aa4d
sergey_malykh <sergeymalykhdm@gmail.com>
2025-04-22 15:22:54 +0600

    feat: обновить страницу поиска

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,12 +1,37 @@
-import tenderStorage from "../../models/Tender/TenderStorage"
-import SearchPage from "./ClientPage"
 import {Metadata} from "next";
+import styles from "@/app/search/page.module.css";
+import TenderCard from "@/app/components/TenderCard/TenderCard";
+import TendersSearch from "@/app/components/TendersSearch/TendersSearch";
+import {search_tenders} from "@/models/Tender/TenderService";
+import NavButtons from "@/app/components/TendersSearch/NavButtons";
 
 export const dynamic = 'force-dynamic'
 export const metadata: Metadata = {
     title: 'Поиск',
 }
-export default async function ServerPage() {
-    const tenders = await tenderStorage.getAll()
-    return (<SearchPage tendersJSON={JSON.stringify(tenders)}/>)
+export default async function ServerPage({searchParams,}: {
+    searchParams: Promise<{ [key: string]: string | undefined }>
+}) {
+    const status = parseInt((await searchParams).status as string) || null
+    const name = (await searchParams).name || ''
+    const company_name = (await searchParams).company_name || ''
+    const reg_number = (await searchParams).reg_number || ''
+    const start = (await searchParams).start || ''
+    const end = (await searchParams).end || ''
+    const page = (await searchParams).page || '0'
+    console.log(status, name, reg_number, company_name, start, end, parseInt(page))
+    const tenders = await search_tenders(status, name, reg_number, company_name, start, end, parseInt(page));
+    return (
+        <main className={styles.content + ' inherit'}>
+            <div id={styles.leftPanel}>
+                <TendersSearch/>
+            </div>
+            <div>
+                <div className={styles.tenders}>
+                    {tenders.map(tender => <TenderCard tender={tender} key={tender.id}/>)}
+                </div>
+                <NavButtons/>
+            </div>
+        </main>
+    )
 }
