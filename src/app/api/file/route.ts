e7cc6163d41997a87eebf1efda1aabfcb0aa4566
sergey_malykh <sergeymalykhdm@gmail.com>
2025-04-29 15:30:17 +0600

    1.3.4

--- src/app/api/file/route.ts
+++ src/app/api/file/route.ts
@@ -1,18 +1,8 @@
-import {NextRequest} from "next/server";
-import {download_file} from "@/app/api/file/download_file";
-import {upload_file} from "@/app/api/file/upload_file";
-import {delete_file} from "@/app/api/file/delete_file";
+import { delete_file } from "@/app/api/file/delete_file";
+import { download_file } from "@/app/api/file/download_file";
+import { upload_file } from "@/app/api/file/upload_file";
 
 export const config = {api: {bodyParser: false}}
-
-export async function GET(request: NextRequest) {
-    return download_file(request)
-}
-
-export async function POST(request: Request) {
-    return upload_file(request)
-}
-
-export async function DELETE(request: NextRequest) {
-    return delete_file(request);
-}
+export const GET = download_file;
+export const POST = upload_file;
+export const DELETE = delete_file;
--- tests/login-fixture.js
+++ tests/login-fixture.js
@@ -4,8 +4,8 @@ export { expect } from '@playwright/test';
 export const test = base.extend({
   page: async ({ page }, use) => {
     await page.goto('http://127.0.0.1:3000/login');
-    await page.fill('input[name="name"]', '123');
-    await page.fill('input[name="password"]', '123');
+    await page.fill('input[name="name"]', 'SuperUser');
+    await page.fill('input[name="password"]', 'SuperPassword');
     await page.click('button:has-text("Войти")');
     await page.waitForURL('http://127.0.0.1:3000/');
     await use(page);
